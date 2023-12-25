7c55c7c1f9c94977400afb3582bf40f3bc6874ea
scriptoriumwebdev <scriptorium.dev22@gmail.com>
2023-12-25 21:10:20 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
           </div>
           <Footer />
           {/* <CookieBanner /> */}
-          <Analytics />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
