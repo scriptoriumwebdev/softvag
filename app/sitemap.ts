fa22d98d4c7272935f04030a808544e32aef44bb
scriptoriumwebdev <scriptorium.dev22@gmail.com>
2023-12-25 22:47:43 +0100

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://softvag.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://softvag.com/kontakt",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+}
