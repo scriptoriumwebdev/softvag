aa66548bf07a866692373301f507d53a36f01b2c
scriptoriumwebdev <scriptorium.dev22@gmail.com>
2024-09-04 11:26:25 +0200

    Change revalidation schema

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -15,21 +15,26 @@ export async function POST(request: NextRequest): Promise<Response> {
     console.log(`model`, body.model);
     if (body.model === "main-page") {
       console.log(`revalidate /`, body.entry);
-      revalidatePath(`/`);
+      revalidatePath(`/`, "page");
     }
-    if (body.model === "about-page") revalidatePath(`/o-firmie`);
+    if (body.model === "about-page") revalidatePath(`/o-firmie`, "page");
     if (body.model === "adaptation-page")
-      revalidatePath(`/kodowania-i-adaptacje-funkcjonalnosci-samochodu`);
+      revalidatePath(
+        `/kodowania-i-adaptacje-funkcjonalnosci-samochodu`,
+        "page"
+      );
     if (body.model === "modification-page")
       revalidatePath(
-        `/modyfikacje-oprogramowania-skrzyn-biegow-DSG-i-S-Tronic`
+        `/modyfikacje-oprogramowania-skrzyn-biegow-DSG-i-S-Tronic`,
+        "page"
       );
-    if (body.model === "faq-page") revalidatePath(`/pytania-i-odpowiedzi`);
-    if (body.model === "opinions-page") revalidatePath(`/opinie`);
-    if (body.model === "contact-page") revalidatePath(`/kontakt`);
+    if (body.model === "faq-page")
+      revalidatePath(`/pytania-i-odpowiedzi`, "page");
+    if (body.model === "opinions-page") revalidatePath(`/opinie`, "page");
+    if (body.model === "contact-page") revalidatePath(`/kontakt`, "page");
     if (body.model === "blog" && body.entry && "slug" in body.entry) {
-      revalidatePath(`/blog/${body.entry.slug}`);
-      revalidatePath(`/blog`);
+      revalidatePath(`/blog/${body.entry.slug}`, "layout");
+      revalidatePath(`/blog`, "page");
     }
     return new Response(null, { status: 204 });
   } else {
