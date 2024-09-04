6c89a012613337b14ab3bcf110a7856dbc8f470b
scriptoriumwebdev <scriptorium.dev22@gmail.com>
2024-09-04 16:41:47 +0200

    change GTM id

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,14 +37,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PV2GFGSJ');
+})(window,document,'script','dataLayer','GTM-5VJFVJN');
                 `}
         </Script>
       </head>
       <body className={inter.className}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PV2GFGSJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5VJFVJN"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
