e5a76d6cb8ca143c26a666c8d67a3d3488ec85a3
scriptoriumwebdev <scriptorium.dev22@gmail.com>
2024-09-04 15:50:50 +0200

    Change GA to GMT

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,27 +31,23 @@ export default function RootLayout({
   return (
     <html lang="pl" suppressHydrationWarning>
       <head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-Y58F5YNVX4`}
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="GMT" strategy="afterInteractive">
           {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
-                });
-                
-                gtag('config', 'G-Y58F5YNVX4', {
-                    page_path: window.location.pathname,
-                });
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PV2GFGSJ');
                 `}
         </Script>
       </head>
       <body className={inter.className}>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PV2GFGSJ"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
         <Providers>
           <Suspense fallback={<Loading />}>
             <Links />
